d8a2bd51072b48f1ac9a25b078a060cf98547920
Nicholas Jacobs <nicholas.jacobs@maine.edu>
2023-05-03 16:45:20 +0000

    Prints messages and checks if already downloaded

--- download.sh
+++ download.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-curl -O https://archive.ics.uci.edu/ml/machine-learning-databases/spambase/spambase.zip
-unzip spambase.zip -d spambase
-rm spambase.zip
+
+if [ ! -d "spambase" ]; then
+  echo "Downloading spambase data..."
+  curl -O https://archive.ics.uci.edu/ml/machine-learning-databases/spambase/spambase.zip
+  echo "Extracting spambase data..."
+  unzip spambase.zip -d spambase
+  rm spambase.zip
+else
+  echo "Spambase data already downloaded."
+fi
+
